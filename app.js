6ea1f33a3ab39c5cf337298dd6c68c84bbf5d335
Lakshay Jain <lakshayjain20@cse.iiitp.ac.in>
2024-02-24 10:28:30 +0000

    completed Dice Game

--- app.js
+++ app.js
@@ -12,11 +12,20 @@ function clearCanvas(canvas){
     const ctx=canvas.getContext('2d')
     ctx.clearRect(0,0,canvas.width,canvas.height)
 }
+
+function diagonal(){
+    createCircle(canvas,30,30,20)
+    createCircle(canvas,170,170,20)
+}
+function center(){
+    createCircle(canvas,100,100,20)
+}
+function anti_diagonal(){
+    createCircle(canvas,30,170,20)
+    createCircle(canvas,170,30,20)
+}
 function draw(canvas,num){
-    console.log("This is working")
     clearCanvas(canvas)
-    console.log(num)
-    // console.log(canvas)
     switch (num){
         case 1:
             createCircle(canvas,100,100,20)
@@ -58,22 +67,51 @@ function draw(canvas,num){
 
 function generateRandom(){
     let z=-1;
-    z=Math.floor((Math.random()*100)%6)
-    if(z==0)z=6;
+    z=Math.floor((Math.random()*100)%6)+1;
     return z;
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
 button = document.getElementById("button")
-button.addEventListener('click',() => {
-    const canvas = document.getElementById("canvas2");
+button.addEventListener('click',async () => {
+    // const canvas = document.getElementById("canvas2");
+    heading = document.getElementById('heading')
+    heading.innerText="Dice Game";
     console.log("This is clicked");
     
-    draw(canvas,generateRandom())
+    const canvas1=document.getElementById("canvas1")
+    const canvas2=document.getElementById("canvas2")
+    var j=0;
+    var a=0,b=0;
+    while(j<10){
+        console.log("Working")
+        draw(canvas1,a=generateRandom())
+        draw(canvas2,b=generateRandom())
+        await sleep(300)
+        console.log(j)
+        j++;
+    }
+    switch((a>b)-(a<b)){
+        case 1:
+            heading.innerText="Player A Wins"
+            break;
+        case -1:
+            heading.innerText="Player B Wins"
+            break;
+        default:
+            heading.innerText="Both Player Wins"
+            break;
+    }
+
 })
 
-window.addEventListener('onload',() => {
-    canvas1=document.getElementById("canvas1")
-    canvas2=docuemt.getElementById("canvas2")
-    console.log("Getting Window Refresh")
-    draw(canvas1,generateRandom())
-    draw(canvas2.generateRandom())
-})
+  
+//   async function demo() {
+//     setTimeout("alert('this alert is timedout and should be the first');", 5000);
+//     // await sleep(5000);
+//     alert('this should be the second one');
+//   }
+//   demo();
